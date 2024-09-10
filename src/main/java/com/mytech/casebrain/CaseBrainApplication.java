5123b2196d3e8183ab30cddb62dd8201c143d412
ProgrammerBSeren <programmerb.serengeti@gmail.com>
2024-09-10 13:36:16 -0400

    ticket1: add initial code

---
+++ src/main/java/com/mytech/casebrain/CaseBrainApplication.java
@@ -0,0 +1,14 @@
+package com.mytech.casebrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseBrainApplication.class, args);
+		System.out.println("CaseBrainApplication is running");
+	}
+
+}
